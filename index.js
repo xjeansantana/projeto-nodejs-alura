8f337880ba7e55200ab97fbebedea73c828494cd
xjeansantana <51293417+xjeansantana@users.noreply.github.com>
2022-03-16 13:49:35 -0300

    Isolando o config do express e usando consign

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const express = require('express')
+const customExpress = require('./config/customExpress')
 
-const app = express()
+const app = customExpress()
 
-app.listen(3000, ()=> console.log('Servidor rodando'))
-
-app.get('/atendimentos', (req, res) => res.send('Você está em atendimentos'))
+app.listen(3000, () => console.log('servidor rodando na porta 3000'))
