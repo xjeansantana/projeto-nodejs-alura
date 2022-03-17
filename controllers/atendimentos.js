5cd298c12cd7d1466c826ce89fb34e81f3320dee
xjeansantana <51293417+xjeansantana@users.noreply.github.com>
2022-03-17 16:10:34 -0300

    Connectando mysql

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,9 @@
-module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está em atendimentos'))
+module.exports = app => { 
+    app.get('/atendimentos', (req, res) => res.send('Você está na  rota de atendimentos e está realizando um GET'))
 
-    app.post('/atendimentos', (req, res) => res.send('Você está na rota de atendimento e está relizando um Post'))
+    app.post('/atendimentos', (req, res) => { 
+        console.log(req.body)
+        res.send('Você está na rota de atendimentos e está  realizando um POST')
+    })
+
 }
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
 
-const app = customExpress()
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('Banco conectado com sucesso')
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+        const app = customExpress()
+
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
