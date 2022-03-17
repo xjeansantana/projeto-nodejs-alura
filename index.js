2a88b6aff072f1a9aaa0c3c2f77cdf3d8143157b
xjeansantana <51293417+xjeansantana@users.noreply.github.com>
2022-03-17 16:35:52 -0300

    Crianda tabela e inserindo no banco

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
+
 
 conexao.connect(erro => {
     if(erro) {
@@ -7,6 +9,7 @@ conexao.connect(erro => {
     } else {
         console.log('Banco conectado com sucesso')
 
+        Tabelas.init(conexao)
         const app = customExpress()
 
         app.listen(3000, () => console.log('servidor rodando na porta 3000'))
