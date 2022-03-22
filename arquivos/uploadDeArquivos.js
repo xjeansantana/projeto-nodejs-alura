3a582cd75e850a042b15534541ad40ad7e7dec77
xjeansantana <51293417+xjeansantana@users.noreply.github.com>
2022-03-22 14:48:14 -0300

    Validação tipos de imagens

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,10 +1,21 @@
 const fs = require('fs')
 
-module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => 
-{
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
+const path = require('path')
 
-    fs.createReadStream(caminho)
-    .pipe(fs.createWriteStream(novoCaminho))
-    .on('finish', () => callbackImagemCriada(novoCaminho))
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) =>
+{
+    const tiposValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))
+
+    if(tipoEhValido == -1){
+        console.log('Erro! Tipo inválido')
+    } else {
+      const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+
+        fs.createReadStream(caminho)
+        
+        .pipe(fs.createWriteStream(novoCaminho))
+        .on('finish', () => callbackImagemCriada(novoCaminho))
+    }
 }
